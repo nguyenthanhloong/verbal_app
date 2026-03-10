45bbaa3cca90f2e0ae39bc386f5cabb71156c2f8
nguyenthanhloong <ngthanhlong.cntt@gmail.com>
2026-03-10 11:06:44 +0700

    Force Vercel to include app folder and update rewrites

--- api/index.py
+++ api/index.py
@@ -1,13 +1,1 @@
-
-
-import os
-import sys
-
-# Lấy đường dẫn tuyệt đối của thư mục 'api' hiện tại
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Ép Python phải ưu tiên tìm kiếm code trong thư mục 'api' này trước
-sys.path.insert(0, current_dir)
-
-# Bây giờ Python đã nhìn thấy thư mục 'app' nằm ngay cạnh index.py
 from app.main import app
